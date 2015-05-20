187d2d4ab849eda80c648f2c93b860573ac90bf4
Michael <[mlorenzo@unrulyvegetables.com]>
2015-05-20 14:43:58 -0700

    credit card challenge complete, working on challenges 1 and 2 next

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,3 +1,55 @@
 (function() {
+var newHeader = document.getElementsByTagName('h1')[0];
+var newTitle = document.getElementsByTagName('title')[0];
+var test = document.getElementsByTagName('p')[0];
+var newForm = document.createElement('form');
+var newDiv = document.createElement('div');
+var divOptions = document.createElement('label');
+var divList = document.createElement('select');
+var submitButton = document.createElement('input');
+//var submitButton = document.getElementsByTagName('input');
 
+// newHeader.innerHTML = "Blaaaaaaarg!!!";
+// test.innerHTML = "My first post is pure bullshit";
+
+var makeDiv = function(newLabel, newInput, inputName) {
+	var newDiv = document.createElement('div');
+
+	var labelElement = document.createElement('label');
+	labelElement.setAttribute('for', newLabel);
+
+	var inputElement = document.createElement('input');
+	inputElement.setAttribute('type', newInput);
+	inputElement.setAttribute('name', inputName);
+
+	labelElement.innerHTML = newLabel;
+	inputElement.innerHTML = newInput;
+
+	newDiv.appendChild(labelElement);
+	newDiv.appendChild(inputElement);
+	newForm.appendChild(newDiv);
+
+	return makeDiv;
+};
+
+makeDiv('Email: ', 'text', 'email');
+makeDiv('Credit Card Number: ', 'text', 'cc');
+makeDiv('Card Expiration Date: ', 'text', 'expiration');
+makeDiv('Name on Card: ', 'text', 'cardName');
+makeDiv('CVC Code: ', 'text', 'cvc');
+makeDiv('City: ', 'text', 'city');
+
+newHeader.innerHTML = "Gimme your personal info so I can rob you"
+
+document.body.appendChild(newForm);
+
+var submitButton = document.createElement('input');
+submitButton.setAttribute('type', 'submit');
+submitButton.setAttribute('id', 'button');
+submitButton.setAttribute('value', 'Press Me!')
+
+newForm.appendChild(submitButton);
+
+//need to figure out how to get the submit button to send info
+//submitButton.onClick
 })();
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'My Blog!' });
+  res.render('index', { title: 'This is a blog!' });
 });
 
 router.get('/pay', function(req, res) {
